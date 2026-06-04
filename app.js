729988e9acb5a900ea66062016a94e93d3bb917f
elie-jerdak <jardakelie@gmail.com>
2026-06-04 11:25:11 +0300

    Changed hompeage layout

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ dotenv.config();
 
 const app = express();
 
+app.use("/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+
 app.use(session({
     secret: process.env.SESSION_SECRET || 'secret',
     resave: false,
